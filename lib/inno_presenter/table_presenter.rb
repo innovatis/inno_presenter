0756ebceb0ffc62f244940af0a7865eae159a011
Burke Libbey <burke@burkelibbey.org>
2011-02-02 17:58:42 -0600

    update

--- lib/inno_presenter/table_presenter.rb
+++ lib/inno_presenter/table_presenter.rb
@@ -1,6 +1,26 @@
 module InnoPresenter
   class TablePresenter < Base
     include Filterriffic::Presenter
+    
+    def filters
+      []
+    end 
+    
+    def columns
+      []
+    end 
+    
+    def present(obj)
+      [
+        # ... ,
+        {}
+      ]
+    end 
+
+    def search_column
+      nil
+    end 
+
   end 
 end 
 
