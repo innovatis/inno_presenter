92320dc5843cc14d507d98959cd3d09fff3f5675
Burke Libbey <burke@burkelibbey.org>
2011-02-02 12:49:47 -0600

    fix

--- lib/inno_presenter/action_controller_extensions.rb
+++ lib/inno_presenter/action_controller_extensions.rb
@@ -1,8 +1,10 @@
 class ActionController::Base
   def self.present_with(presenter)
+    define_method(:presenter) do
+      @presenter ||= presenter.new(self)
+    end 
     define_method(:index) do
-      @presenter    = presenter.new(self)
-      @presentation = @presenter.present_collection(collection)
+      @presentation = self.presenter.present_collection(collection)
     end 
   end 
 end 
