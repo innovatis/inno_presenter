5b719d02f48f193fc5e329cddafb3d946520b639
Burke Libbey <burke@burkelibbey.org>
2011-02-07 18:57:10 -0600

    foo

--- lib/inno_presenter.rb
+++ lib/inno_presenter.rb
@@ -2,6 +2,9 @@ module InnoPresenter
 
   class Railtie < Rails::Railtie
     initializer 'inno_presenter.initialize' do |app|
+      assets_path = File.join(File.dirname(__FILE__), '..', 'assets')
+      FileUtils.ln_s(File.expand_path(assets_path), File.join(Rails.root, 'public', 'inno_presenter'))
+
       app.config.load_paths += ["#{Rails.root}/app/presenters/"]
     end
   end
