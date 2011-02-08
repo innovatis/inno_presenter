c3b8001b0e42bc59075a7619c9e5dd35cedf153d
Burke Libbey <burke@burkelibbey.org>
2011-02-07 19:12:09 -0600

    foo

--- lib/inno_presenter.rb
+++ lib/inno_presenter.rb
@@ -6,6 +6,20 @@ module InnoPresenter
       FileUtils.ln_s(File.expand_path(assets_path), File.join(Rails.root, 'public', 'inno_presenter'))
 
       app.config.load_paths += ["#{Rails.root}/app/presenters/"]
+
+      javascripts = {
+        :inno_presenter => [
+          '/inno_presenter/jquery.dataTables.js'
+        ]
+      }
+      
+      stylesheets = { 
+        :inno_presenter => []
+      }
+
+      ActionView::Helpers::AssetTagHelper.register_javascript_expansion javascripts
+      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion stylesheets
+      
     end
   end
 
